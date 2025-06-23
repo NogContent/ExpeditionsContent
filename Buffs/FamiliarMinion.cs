90cb36e2b035a963131e6f0f2cda26cce742af00
NogContent <estothshar@hotmail.com>
2025-06-23 08:16:47 -0400

    Add files via upload

--- Buffs/FamiliarMinion.cs
+++ Buffs/FamiliarMinion.cs
@@ -1,39 +1,40 @@
-﻿using Terraria;
+﻿using ExpeditionsContent144.Projs.Familiars;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ExpeditionsContent.Buffs
+namespace ExpeditionsContent144.Buffs
 {
-    class FamiliarMinion : ModBuff
-    {
-        public override void SetDefaults()
-        {
-            DisplayName.SetDefault("Familiar");
-            Description.SetDefault("The familiar will fight for you");
-            Main.buffNoSave[Type] = true;
-            Main.buffNoTimeDisplay[Type] = true;
-        }
-        
-        public override void Update(Player player, ref int buffIndex)
-        {
-            PlayerExplorer modPlayer = player.GetModPlayer<PlayerExplorer>(mod);
-            int minionCount = 0;
-            minionCount += player.ownedProjectileCounts[mod.ProjectileType("MinionFox")];
-            minionCount += player.ownedProjectileCounts[mod.ProjectileType("MinionChicken")];
-            minionCount += player.ownedProjectileCounts[mod.ProjectileType("MinionCat")];
-            if (minionCount > 0)
-            {
-                modPlayer.familiarMinion = true;
-            }
-            if (!modPlayer.familiarMinion)
-            {
-                player.DelBuff(buffIndex);
-                buffIndex--;
-            }
-            else
-            {
-                player.buffTime[buffIndex] = 18000;
-            }
-        }
-    }
+	class FamiliarMinion : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			/****DisplayName.SetDefault("Familiar");
+            Description.SetDefault("The familiar will fight for you");*/
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			PlayerExplorer modPlayer = player.GetModPlayer<PlayerExplorer>();
+			int minionCount = 0;
+			minionCount += player.ownedProjectileCounts[ModContent.ProjectileType<MinionFox>()];
+			minionCount += player.ownedProjectileCounts[ModContent.ProjectileType<MinionChicken>()];
+			minionCount += player.ownedProjectileCounts[ModContent.ProjectileType<MinionCat>()];
+			if (minionCount > 0)
+			{
+				modPlayer.familiarMinion = true;
+			}
+			if (!modPlayer.familiarMinion)
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+			else
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+		}
+	}
 }
