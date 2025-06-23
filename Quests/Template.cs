cd5b9a1b29afef5b53694582923b4ad7a760fadc
NogContent <estothshar@hotmail.com>
2025-06-23 08:10:55 -0400

    Add files via upload

--- Quests/Template.cs
+++ Quests/Template.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 using Terraria;
 using Terraria.ID;
-using Expeditions;
+using Expeditions144;
 
-namespace ExpeditionsContent.Quests.Daily
+namespace ExpeditionsContent144.Quests.Daily
 {
-    class Template : ModExpedition
-    {
-        public override void SetDefaults()
-        {
-            expedition.name = "NAME";
-            SetNPCHead(NPCID.Guide, false);
-            expedition.difficulty = 0;
-            expedition.ctgExplore = true;
-        }
-        public override void AddItemsOnLoad()
-        {
-        }
-        public override string Description(bool complete)
-        {
-            return "DESCRIPTION";
-        }
+	class Template : ModExpedition
+	{
+		public override void SetDefaults()
+		{
+			expedition.name = "NAME";
+			SetNPCHead(NPCID.Guide, false);
+			expedition.difficulty = 0;
+			expedition.ctgExplore = true;
+		}
+		public override void AddItemsOnLoad()
+		{
+		}
+		public override string Description(bool complete)
+		{
+			return "DESCRIPTION";
+		}
 
-        public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
-        {
-            return false;
-        }
-    }
+		public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+		{
+			return false;
+		}
+	}
 }
